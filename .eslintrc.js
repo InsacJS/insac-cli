53a9de33682719042635d1d254de44607946a6c9
Wilmer Alex Quispe Quispe <q.wilmer.alex@gmail.com>
2018-02-06 23:26:18 -0400

    Configuración inicial

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,34 +4,8 @@ module.exports = {
     "commonjs": true,
     "es6": true
   },
-  "extends": "eslint:recommended",
+  "extends": "standard",
   "parserOptions": {
     "ecmaVersion": 2017
-  },
-  "rules": {
-    "no-case-declarations": "error",
-    "no-path-concat": "error",
-    "no-undef": "error",
-    "no-console": [
-      "error",
-      { allow: ["log", "warn", "error"] }
-    ],
-    "indent": [
-      "error",
-      2
-    ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "single",
-      { "allowTemplateLiterals": true }
-    ],
-    "semi": [
-      "error",
-      "never"
-    ]
   }
 }
