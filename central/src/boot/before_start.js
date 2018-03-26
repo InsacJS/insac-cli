4670ea05fcc5054edc069a3caa0f58556f990c7d
Wilmer Alex Quispe Quispe <q.wilmer.alex@gmail.com>
2018-03-26 16:12:12 -0400

    Templates actualizados.

---
+++ central/src/boot/before_start.js
@@ -0,0 +1,30 @@
+const _ = require('lodash')
+
+module.exports = (app) => {
+  // |=============================================================|
+  // |---- TAREAS A REALIZAR ANTES DE INICIALIZAR LOS MÓDULOS -----|
+  // |=============================================================|
+
+  // Controla el método OPTIONS
+  app.use((req, res, next) => {
+    if (req.method === 'OPTIONS') {
+      return res.status(200).send('ok')
+    }
+    return next()
+  })
+
+  // Muestra las peticiones por consola
+  if (process.env.LOGGER_REQ && process.env.LOGGER_REQ === 'true') {
+    app.use((req, res, next) => {
+      const METHOD     = req.method
+      const PATH_NAME  = req._parsedUrl.pathname
+      const QUERY      = req._parsedUrl.search ? `\n\x1b[2mquery = ${req._parsedUrl.search}\x1b[0m` : ''
+      const BODY       = Object.keys(req.body).length > 0 ? `\n\x1b[2mbody = ${JSON.stringify(req.body, null, 2)}\x1b[0m` : ''
+      process.stdout.write(`\n${_.padEnd(`[${METHOD}]`, 8, ' ')} ${PATH_NAME}${QUERY}${BODY}\n`)
+      return next()
+    })
+  }
+
+  // Importa los campos personalizados
+  require('../tools/Field.js')
+}
---
+++ central/src/config/index.js
@@ -0,0 +1,56 @@
+// const fs     = require('fs')
+// const path   = require('path')
+
+const LOGGING = sql => process.stdout.write(`\n\x1b[2m${sql}\x1b[0m\n`)
+
+// |=============================================================|
+// |------------ CONFIGURACIÓN PARA LA BASE DE DATOS ------------|
+// |=============================================================|
+
+exports.DATABASE = {
+  username : process.env.DB_USER || 'postgres',
+  password : process.env.DB_PASS || '12345678',
+  database : process.env.DB_NAME || 'insac_central',
+  params   : {
+    dialect          : 'postgres',
+    host             : process.env.DB_HOST_NAME || '127.0.0.1',
+    port             : process.env.DB_HOST_PORT || '5432',
+    timezone         : '-04:00',
+    lang             : 'es',
+    logging          : (process.env.LOGGER_SQL && process.env.LOGGER_SQL === 'true') ? LOGGING : false,
+    operatorsAliases : false,
+    define           : {
+      underscored     : true,
+      freezeTableName : true,
+      timestamps      : true,
+      paranoid        : true,
+      createdAt       : '_fecha_creacion',
+      updatedAt       : '_fecha_modificacion',
+      deletedAt       : '_fecha_eliminacion'
+    }
+  }
+}
+
+// |=============================================================|
+// |------------ CONFIGURACIÓN PARA EL SERVIDOR -----------------|
+// |=============================================================|
+
+exports.SERVER = {
+  port : process.env.PORT     || 4000,
+  env  : process.env.NODE_ENV || 'development',
+  cors : {
+    'origin'                       : '*',
+    'methods'                      : 'GET,POST,PUT,DELETE,OPTIONS',
+    'preflightContinue'            : true,
+    'Access-Control-Allow-Headers' : 'Authorization,Content-Type,Content-Length'
+  },
+  ssl: {
+    // key  : fs.readFileSync(path.join(process.cwd(), 'certs/privatekey.pem')),
+    // cert : fs.readFileSync(path.join(process.cwd(), 'certs/certificate.pem'))
+  },
+  https: false
+}
+
+// |=============================================================|
+// |------------ CONFIGURACIÓN PARA LOS MÓDULOS -----------------|
+// |=============================================================|
