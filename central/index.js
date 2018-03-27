5048ce84e6b094065fe23fc68713167e4032b0ec
Wilmer Alex Quispe Quispe <q.wilmer.alex@gmail.com>
2018-03-26 23:37:55 -0400

    Se adicionaron templates para crear los componentes del proyecto

--- central/index.js
+++
@@ -1,3 +0,0 @@
-global.insac = require('path').resolve('/home/alex/PROYECTO INSAC/PRODUCTION/lib/insac')
-
-module.exports = require('./src/app.js')
--- central/src/app.js
+++
@@ -1,7 +0,0 @@
-const Insac = require(insac).Insac
-
-const service = new Insac()
-
-service.init()
-
-module.exports = service
